f2f7c61b13d2e18f222063e0d6fdcee2ec059533
Jisun Lee <jisun.bella.lee@gmail.com>
2022-11-21 14:11:38 +1100

    commit contact

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Contact.css'
+
+const Contact = () => {
+  return (
+    <section id='contact' className='container contact__container'>
+      <h3 className='title'>CONTACT</h3>
+      <div className="contact__content">
+        phone
+        email
+
+      </div>
+    </section>
+  )
+}
+
+export default Contact
