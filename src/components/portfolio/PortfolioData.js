06c89d98b77053a3db8bfbf54154bc566a034090
Jisun Lee <jisun.bella.lee@gmail.com>
2022-11-28 10:05:11 +1100

    update project data

--- src/components/portfolio/PortfolioData.js
+++ src/components/portfolio/PortfolioData.js
@@ -17,31 +17,31 @@ const data = [
     title: 'Light and Salt',
     imgSrc: [lightAndSaltMain],
     category: 'personal',
-    desc: 'A website for finding and rating restaurants',
+    desc: 'Website for finding and rating restaurants',
     stack: 'HTML ・ CSS ・ JavaScript ・ jQuery ・ Bootstrap ・ Java ・ Spring Framework ・ MySQL'
   },
   {
     id: 2,
-    title: 'A Drone Monitoring Solution',
+    title: 'Drone Monitoring Solution',
     imgSrc: [eagleEyeMain],
     category: 'work',
-    desc: 'A solution for tracking and monitoring drones at Incheon International Airport',
+    desc: 'Solution for tracking and monitoring drones at Incheon International Airport',
     stack: 'HTML ・ CSS ・ JavaScript ・ jQuery ・ OpenLayers ・ Leaflet ・ C# ・ .Net Core ・ SignalR ・ RabbitMQ ・ SQL Server'
   },
   {
     id: 3,
-    title: 'A Server Monitoring Website',
+    title: 'Server Monitoring Website',
     imgSrc: [artPortalMain, artPortalLogin, artPortalSearch],
     category: 'work',
-    desc: 'A solution for monitoring LDAP Server and Exchange Server and saving logs and data',
+    desc: 'Solution for monitoring LDAP Server and Exchange Server and saving logs and data',
     stack: 'HTML ・ CSS ・ JavaScript ・ jQuery ・ C# ・ .Net Core ・ PowerShell ・ SignalR ・ RabbitMQ ・ SQL Server'
   },
   {
     id: 4,
-    title: 'An E-Commerce Website for Drone',
+    title: 'E-Commerce Website for Drone',
     imgSrc: [dmiMain, dmiCategory, dmiDetail],
     category: 'work',
-    desc: 'A solution for E-commerce website for selling hydrogen drones and battery',
+    desc: 'E-commerce website for selling hydrogen drones and battery',
     stack: 'HTML ・ CSS ・ Vue.js ・ Nuxt.js ・ C# ・ .Net Core ・ SQL Server'
   },
   // {
@@ -54,10 +54,10 @@ const data = [
   // },
   {
     id: 5,
-    title: 'A Website for Questions and Answers',
+    title: 'Website for Questions and Answers',
     imgSrc: [ftscMain, ftscLogin, ftscSignUp, ftscBoard],
     category: 'work',
-    desc: 'A website for clients and their customers for QnA',
+    desc: 'Website for clients and their customers for QnA',
     stack: 'HTML ・ CSS ・ JavaScript ・ jQuery ・ C# ・ .Net Framework ・ SQL Server'
   }
 ]
