b37ce96e7d9387f6ce6fb7c01d38072517c9ad5e
Jisun Lee <jisun.bella.lee@gmail.com>
2023-09-11 14:42:15 +0900

    update responsible

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,22 +2,14 @@ import React, { useState, useEffect } from 'react';
 import './Home.css';
 
 const Home = () => {
-  const [isVisible, setVisible] = useState(false);
 
-  // const _fadeIn = () => {
-  //   setVisible(true);
-  // }
-
-  // window.addEventListener('load', _fadeIn);
-  // window.addEventListener('scroll', _fadeIn);
-  
 
 
 
   return (
     <section id="home">
       <div className='container'>
-        <div className={`home-content fade-in-section ${isVisible ? 'is-visible' : 'is-visible'}`}>
+        <div className='home-content'>
           <h1>Hello, I am <strong>Jisun Lee</strong>.<br /> A <strong>Frontend </strong> Web Developer based in Korea.</h1>
         </div>
       </div>
