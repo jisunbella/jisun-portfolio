b28b5dd36de8ee23e17824aa8e7131eff5015e24
jslee <easysun93@gmail.com>
2025-04-09 00:15:04 +0900

    Design: navbar 모바일인 경우에만 보이도록 수정

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,6 +1,6 @@
 export default function NavBar() {
   return (
-    <header className="fixed top-0 w-full h-16 bg-white z-40 flex  items-center px-6">
+    <header className="lg:hidden fixed top-0 w-full h-16 bg-white z-40 flex items-center px-6">
       <h1 className="text-xl font-bold">Jisun Lee</h1>
     </header>
   );
